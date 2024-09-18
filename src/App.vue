cc0d8c5a898c619f8682dd345c9ffc811d64aae9
Vyacheslav <lakalut3868@gmal.com>
2024-09-18 23:12:10 +0300

    Added styles and markup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="app">
+		<Sidebar />
+		<Chat />
+	</div>
+</template>
+
+<script>
+import Chat from './components/Chat.vue'
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+	components: {
+		Sidebar,
+		Chat,
+	},
+}
+</script>
+
+<style scoped>
+.app {
+	display: flex;
+	height: 100vh;
+}
+</style>
